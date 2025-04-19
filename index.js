1dbc2c0f13727e26f11037c6e1630676424c3b4e
Natejpg <nathan11.abreu@gmail.com>
2025-04-18 21:14:53 -0300

    Finalizando curso de backend da FreeCodeCamp.

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var cors = require('cors');
+const path = require('path');
+const multer = require('multer');
 require('dotenv').config()
 
 var app = express();
+const upload = multer({dest: 'uploads/' });
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -10,11 +13,27 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
-
-
-
+ 
 
 const port = process.env.PORT || 3000;
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+
+    if(!req.file){
+      return res.status(400).json({error: 'No file uploaded' });
+    }
+
+    res.json({
+
+      name: req.file.originalname,
+      type: req.file.mimetype,
+      size: req.file.size,
+      
+    })
+    
+})
+
+
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
 });
